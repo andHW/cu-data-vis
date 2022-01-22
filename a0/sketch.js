b28a9303ea89e8b255ac895ab2fefc95bbeba6ae
Andreas <and@reas.me>
2022-01-23 04:31:56 +0800

    add basics

--- a0/sketch.js
+++ a0/sketch.js
@@ -1,14 +1,62 @@
 function setup() {
-    createCanvas(800, 600); // make an HTML canvas element width x height pixels
+    createCanvas(windowWidth, windowHeight);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function drawTimeText() {
+    let tSize = 40;
+    let tColor = 'rgba(0, 0, 0, .05)';
+    let timeText = [hour(), minute(), second()]
+        .map(n => String(n).padStart(2, '0'))
+        .join(":");
+    let tWidth = (windowWidth - textWidth(timeText)) / 2;
+    let tHeight = windowHeight / 2 + tSize / 4;
+    textSize(tSize);
+    fill(tColor);
+    text(timeText, tWidth, tHeight);
+}
+
+function drawTimeMouse() {
+    let pSize = 20;
+    let pColor = 'rgba(0,0,0, .8)';
+    let mx = mouseX;
+    let my = mouseY;
+    if (mx <= 0 || mx >= windowWidth) {
+        mx = windowWidth / 2;
+    }
+    if (my <= 0 || my >= windowHeight) {
+        my = windowHeight / 2;
+    }
+
+    noStroke();
+    fill(pColor);
+    ellipse(mx, my, pSize);
+}
+
+function drawBg() {
+    let itv = 5;
+    let cs = ['#AAF', '#AFA', '#AFF', '#FAA', '#FAF', '#FFA', '#FFF'];
+    let ci = Math.round(second() / itv) % cs.length;
+    let cc = cs[ci];
+    background(cc);
 }
 
 function draw() {
-    background(225);
-    textSize(32);
-    fill(180);
-    text(hour(), 10, 30);
-    fill(100);
-    text(minute(), 10, 60);
-    fill(0);
-    text(second(), 10, 90);
+    drawBg();
+    drawTimeText();
+    drawTimeMouse();
 }
+
+/*
+text center maths 😵‍💫😵‍💫😵‍💫
+otl = object top left
+ow  = object width
+sw  = screen width
+otl + ow/2 = sw/2
+otl = (sw-ow)/2
+-----------
+   -----   
+*/
